b4a3a65ae3e1ab2b3686ae96facb230c79210ffd
serebrianskii <serebrianskiik@gmail.com>
2023-11-06 21:05:54 +0100

    Add main class and test prototypes

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(projectStart());
+    }
+
+    public static String projectStart() {
+        String text = "Project start test";
+        return text;
+    }
+}
---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    public void mainTest() {
+        String expected = "Project start test";
+        String actual = App.projectStart();
+        assertEquals(expected, actual);
+    }
+}
