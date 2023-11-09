699a327033e5c92b8c049ea34d51d9a3f39be71a
serebrianskii <serebrianskiik@gmail.com>
2023-11-09 21:13:43 +0100

    Refactor main class App to include validator

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,20 @@
 package hexlet.code;
 
+import hexlet.code.schemas.StringSchema;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(projectStart());
-    }
+        Validator v = new Validator();
+        StringSchema schema = v.string().required().minLength(5).contains("hex");
 
-    public static String projectStart() {
-        String text = "Project start test";
-        return text;
+        System.out.println(schema.isValid(null));
+        System.out.println(schema.isValid(""));
+        System.out.println(schema.isValid("hexa"));
+        System.out.println(schema.isValid("huehuehue"));
+        System.out.println(schema.isValid("hexlet"));
+
+        StringSchema schema2 = v.string();
+        System.out.println(schema2.isValid(null));
     }
 }
+
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,15 +1,15 @@
-package hexlet.code;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-class AppTest {
-    @Test
-    public void mainTest() {
-        String expected = "Project start test";
-        String actual = App.projectStart();
-        assertEquals(expected, actual);
-    }
-}
+//package hexlet.code;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//
+//class AppTest {
+//    @Test
+//    public void mainTest() {
+//        String expected = "Project start test";
+//        String actual = App.projectStart();
+//        assertEquals(expected, actual);
+//    }
+//}
