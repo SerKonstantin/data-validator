dbd49b905207fc656f32f74200d91ad475992d03
serebrianskii <serebrianskiik@gmail.com>
2023-11-10 21:10:21 +0100

    Implement validation functionality for Integer input

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,96 @@
+package hexlet.code.schemas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NumberSchema {
+    private final Map<String, List<Object>> requirements = new HashMap<>();
+
+    public boolean isValid(Object input) {
+        if (!isValidInput(input)) {
+            return false;
+        }
+
+        boolean isAllChecksPass = true;
+
+        for (Map.Entry<String, List<Object>> entry : requirements.entrySet()) {
+            String typeOfCheck = entry.getKey();
+            List<Object> parameters = entry.getValue();
+
+            boolean isCurrentCheckPass = switch (typeOfCheck) {
+                case "required" -> checkRequired(input);
+                case "positive" -> checkPositive(input);
+                case "range" -> parameters.stream().allMatch(parameter -> checkRange(parameter, input));
+                default -> throw new RuntimeException();
+            };
+
+            if (!isCurrentCheckPass) {
+                isAllChecksPass = false;
+            }
+        }
+
+        return isAllChecksPass;
+    }
+
+    // Section with methods to set up checking requirements
+    public NumberSchema required() {
+        requirements.computeIfAbsent("required", value -> new ArrayList<>()).add(true);
+        return this;
+    }
+
+    public NumberSchema positive() {
+        requirements.computeIfAbsent("positive", value -> new ArrayList<>()).add(true);
+        return this;
+    }
+
+    public NumberSchema range(int start, int end) {
+        // Make sure to have range look like "start <= input <= end"
+        if (start > end) {
+            int tmp = start;
+            start = end;
+            end = tmp;
+        }
+
+        int[] range = {start, end};
+        requirements.computeIfAbsent("range", value -> new ArrayList<>()).add(range);
+        return this;
+    }
+
+    // Section with methods to check each condition from requirements
+    private boolean isValidInput(Object input) {
+        return input == null || input instanceof Integer;
+    }
+
+    private boolean checkRequired(Object input) {
+        return !(input == null);
+    }
+
+    private boolean checkPositive(Object input) {
+        if (input == null) {
+            return true;
+        }
+
+        if (!(input instanceof Integer)) {
+            return false;
+        }
+        return (int) input > 0;
+    }
+
+    private boolean checkRange(Object range, Object input) {
+        if (input == null) {
+            return true;
+        }
+
+        if (!(input instanceof Integer) || !(range instanceof int[])) {
+            return false;
+        }
+        int value = (int) input;
+        int[] tmpArr = (int[]) range;
+        int start = tmpArr[0];
+        int end = tmpArr[1];
+
+        return (start <= value && value <= end);
+    }
+}
