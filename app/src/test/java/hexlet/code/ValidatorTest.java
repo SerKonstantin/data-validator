b7db84b6c9bf8c3d59d576f88f3a60ce3b665416
serebrianskii <serebrianskiik@gmail.com>
2023-11-10 20:23:55 +0100

    Add test before implementing NumberSchema for Validator

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidatorTest {
     @Test
-    public void validatorTest() {
+    public void validatorStringTest() {
         Validator v = new Validator();
         StringSchema schema = v.string();
 
@@ -31,4 +32,34 @@ class ValidatorTest {
 
         assertFalse(schema.isValid("some text"));
     }
+
+    @Test
+    public void validatorNumberTest() {
+        Validator v = new Validator();
+        NumberSchema schema = v.number();
+
+        assertFalse(schema.isValid("10"));
+        assertTrue(schema.isValid(0));
+        assertTrue(schema.isValid(null));
+
+        schema.positive();
+
+        assertFalse(schema.isValid(0));
+        assertTrue(schema.isValid(null));
+        assertFalse(schema.isValid(-10));
+        assertTrue(schema.isValid(10));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+
+        schema.range(10, 5);
+        schema.range(4, 9);
+
+        assertTrue(schema.isValid(4));
+        assertTrue(schema.isValid(9));
+        assertFalse(schema.isValid(5));
+        assertFalse(schema.isValid(10));
+    }
+
 }
