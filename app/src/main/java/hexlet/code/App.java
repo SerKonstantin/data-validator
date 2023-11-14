5694b593228e53a9fa81688a66331c54810b7204
serebrianskii <serebrianskiik@gmail.com>
2023-11-14 14:45:03 +0100

    Chore App and AppTest classes to remove unused code

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,20 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.schemas.StringSchema;
-
 public class App {
     public static void main(String[] args) {
         Validator v = new Validator();
-        StringSchema schema = v.string().required().minLength(5).contains("hex");
-
-        System.out.println(schema.isValid(null));
-        System.out.println(schema.isValid(""));
-        System.out.println(schema.isValid("hexa"));
-        System.out.println(schema.isValid("huehuehue"));
-        System.out.println(schema.isValid("hexlet"));
-
-        StringSchema schema2 = v.string();
-        System.out.println(schema2.isValid(null));
+        boolean checkExample = v.number().required().isValid(null);
+        System.out.println(checkExample);
     }
 }
 
